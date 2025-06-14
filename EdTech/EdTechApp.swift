fbdd982e69c1158a139cfb28f059f5f4cf15d501
Yerosh <adilbekernur@gmail.com>
2025-06-14 11:06:22 +0500

    Initial Commit

---
+++ EdTech/EdTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdTechApp.swift
+//  EdTech
+//
+//  Created by Yernur Adilbek on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct EdTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
