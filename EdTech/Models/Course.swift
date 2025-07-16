0daf1f93da003407456f0928df3c1c9e6dae4ba5
Yerosh <adilbekernur@gmail.com>
2025-07-16 18:42:30 +0500

    Added Tabs, HomeView

---
+++ EdTech/Models/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  EdTech
+//
+//  Created by Yernur Adilbek on 16.07.2025.
+//
+import Foundation
+
+struct Course: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+}
--- EdTech/Views/HomeView.swift
+++ EdTech/Views/HomeView.swift
@@ -11,11 +11,25 @@ struct HomeView: View {
     var user: User
     @State private var searchText = ""
     
+    let courses: [Course] = [
+        Course(imageName: "math", title: "Математика"),
+        Course(imageName: "eng", title: "Англиский")
+    ]
+    
+    let recommendedCourses: [Course] = [
+        Course(imageName: "mathBeg", title: "Математика для начинающих"),
+        Course(imageName: "engBeg", title: "Английский для начинающих"),
+        Course(imageName: "math", title: "Математика"),
+        Course(imageName: "eng", title: "Англиский")
+    ]
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
+        VStack(alignment: .leading) {
             Image(user.avatarName)
                 .resizable()
+                .clipShape(Circle())
                 .frame(width: 32, height: 32)
+                .padding(.bottom, 28)
             
             HStack {
                 Image(systemName: "magnifyingglass")
@@ -29,14 +43,65 @@ struct HomeView: View {
             .padding(12)
             .background(Color("E8F0F2"))
             .cornerRadius(12)
+            .padding(.bottom, 32)
             
-            Text("Уроки")
+            Text("Курсы")
                 .font(.custom("Spline Sans", size: 22))
                 .fontWeight(.bold)
                 .foregroundColor(Color("0D171C"))
-                
+                .padding(.bottom, 28)
             
-            Spacer()
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 12) {
+                    ForEach(courses) { item in
+                        VStack(alignment: .leading, spacing: 16) {
+                            Image(item.imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 160, height: 160)
+                                .background(Color.blue.opacity(0.1))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                            
+                            Text(item.title)
+                                .font(.custom("Spline Sans", size: 16))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color("0D171C"))
+                        }
+                        .frame(width: 160)
+                    }
+                }
+            }
+            .padding(.bottom, 28)
+
+            Text("Рекомендаций")
+                .font(.custom("Spline Sans", size: 22))
+                .fontWeight(.bold)
+                .foregroundColor(Color("0D171C"))
+                .padding(.bottom, 28)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 12) {
+                    ForEach(recommendedCourses) { item in
+                        VStack(alignment: .leading, spacing: 16) {
+                            Image(item.imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 160, height: 160)
+                                .background(Color.blue.opacity(0.1))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                            
+                            Text(item.title)
+                                .font(.custom("Spline Sans", size: 16))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color("0D171C"))
+                        }
+                        .frame(width: 160)
+                    }
+                }
+            }
+//            .padding(.bottom, 28)
+            
+//            Spacer()
         
         }
         .padding(.horizontal)
@@ -46,5 +111,5 @@ struct HomeView: View {
 }
 
 #Preview {
-    HomeView(user: User(avatarName: "avatar1", email: "yerosh", password: "qwerty", firstName: "Yernur", lastName: "Adilbek", userType: "adilbekernur@gmail.com"))
+    HomeView(user: User(avatarName: "avatar6", email: "yerosh", password: "qwerty", firstName: "Yernur", lastName: "Adilbek", userType: "adilbekernur@gmail.com"))
 }
